158c80ad8950524908fb3e627d61904f5c4b7b33
Raul <raulblazquezibanez@gmail.com>
2023-10-03 11:13:29 +0200

    martes3/10

--- Teoría/11POO/Gestionar.php
+++ Teoría/11POO/Gestionar.php
@@ -1,5 +1,6 @@
 <?php
 include_once 'claseAlumno.php';
+include_once 'Acceso a datos.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -32,7 +33,20 @@ include_once 'claseAlumno.php';
     //Si pulsamos en Crear Alumno se crea el objeto y se muestra
     if (isset($_POST['crear'])) {
         $a = new Alumno($_POST['numExp'], $_POST['nombre'], strtotime($_POST['fechaN']));
-        $a->mostrar();
+
+        //Chequear si el nº de expediente no existe en el fichero
+        //Si existe, se debe mostrar un error
+        $alTmp = obtenerAlumno($a->getNumExp());
+        if ($alTmp == null) {
+            //Guardar el alumno en el fichero
+            if (crearAlumno($a)) {
+                echo '<p style="color:blue;">Alumno creado:' . $a->mostrar() . '</p>';
+            } else {
+                echo '<p style="color:red;">Error al crear el alumno0</p>';
+            }
+        } else {
+            echo '<p style="color:red;">Error: el alumno :' . $a->getNumExp() . ' ya existe</p>';
+        }
     }
     ?>
 </body>
