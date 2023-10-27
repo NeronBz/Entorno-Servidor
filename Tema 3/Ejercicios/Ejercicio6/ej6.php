53c5ad539ddfa394b0dbb5b8333ef577dd728315
Raul <raulblazquezibanez@gmail.com>
2023-10-27 14:26:23 +0200

    viernes27/10

--- Tema 3/Ejercicios/Ejercicio6/ej6.php
+++ Tema 3/Ejercicios/Ejercicio6/ej6.php
@@ -1,10 +1,10 @@
 <?php
+session_start();
 if (isset($_POST['tirar'])) {
     //Comprobar si hay nombre de jugador
     if (empty($_POST['nombre'])) {
         $mensaje = '<span style="color:red;">*Debes rellenar el nombre del jugador</span>';
     } else {
-        session_start();
         //Recuperar los datos si existen de la sesión
         if (isset($_SESSION['jugadores'])) {
             $jugadores = $_SESSION['jugadores'];
@@ -12,11 +12,12 @@ if (isset($_POST['tirar'])) {
         //Generar nº
         $numero = rand(1, 6);
         //Guardar datos en un array
-        $jugadores[] = array($_POST['nombre'] => $numero);
+        $jugadores[$_POST['nombre']] = $numero;
         //Guardar en sesión
         $_SESSION['jugadores'] = $jugadores;
     }
 } elseif (isset($_POST['borrar'])) {
+    session_unset();
 }
 ?>
 <!DOCTYPE html>
@@ -47,7 +48,7 @@ if (isset($_POST['tirar'])) {
         //Mostrar tiradas
         if (isset($_SESSION['jugadores']))
             foreach ($_SESSION['jugadores'] as $clave => $valor) {
-                echo '<li>' . $clave . '</li>';
+                echo '<li>' . $clave . "=>" . $valor . '</li>';
             }
         ?>
 
