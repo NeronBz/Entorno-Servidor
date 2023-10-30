6b107c2e2eb47d9d8304d6f98bbe6ef50370de53
Raul <raulblazquezibanez@gmail.com>
2023-10-30 10:19:42 +0100

    lunes30/10

---
+++ Tema 3/taller/Pieza/cPieza.php
@@ -0,0 +1,86 @@
+<?php
+require_once '../modelo.php';
+$bd = new Modelo();
+if ($bd->getConexion() == null) {
+    $mensaje = array('e', 'Error, no hay conexión con la bd');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Taller - Gestión de piezas</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <header>
+        <?php
+        require_once '../menu.php';
+        ?>
+    </header>
+    <section>
+        <!-- Crear Piezas -->
+    </section>
+    <section>
+        <!-- Comunicar mensajes -->
+        <?php
+        if (isset($mensaje)) {
+            if ($mensaje[0] == 'e') {
+                echo '<h3 class="text-danger">' . $mensaje[1] . '</h3>';
+            } else {
+                echo '<h3 class="text-success">' . $mensaje[1] . '</h3>';
+            }
+        }
+        ?>
+    </section>
+    <section>
+        <div class="container p-5 my-5 border">
+            <!-- Mostrar piezas y dar opción a modificar y borrar -->
+            <?php
+            if ($bd->getConexion() != null) {
+                //Obtener piezas
+                $piezas = $bd->obtenerPiezas();
+                //Mostramos las piezas en una tabla
+            ?>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Código</th>
+                            <th>Clase</th>
+                            <th>Descripción</th>
+                            <th>Precio</th>
+                            <th>Stock</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        foreach ($piezas as $p) {
+                            $p = new Pieza();
+                            echo '<tr>';
+                            echo  '<td>' . $p->getCodigo() . '</td>';
+                            echo  '<td>' . $p->getClase() . '</td>';
+                            echo  '<td>' . $p->getDescripcion() . '</td>';
+                            echo  '<td>' . $p->getPrecio() . '</td>';
+                            echo  '<td>' . $p->getStock() . '</td>';
+                            echo '</tr>';
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            <?php
+            }
+            ?>
+        </div>
+
+    </section>
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
---
+++ Tema 3/taller/menu.php
@@ -0,0 +1,19 @@
+<!-- Grey with black text -->
+<nav class="navbar navbar-expand-sm bg-light navbar-light">
+    <div class="container-fluid">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link active" href="cPieza.php">Pieza</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+        </ul>
+    </div>
+</nav>
