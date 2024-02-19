31f7b36c04d663485fc6f22f937b83d72967a746
RaulInstituto <rblazquezi02@educarex.es>
2024-02-19 10:18:23 +0100

    lunes19/02

--- u4_ExBiblio/app/Http/Controllers/PrestamoC.php
+++ u4_ExBiblio/app/Http/Controllers/PrestamoC.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Libro;
 use App\Models\Prestamo;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PrestamoC extends Controller
 {
@@ -25,6 +27,9 @@ class PrestamoC extends Controller
     }
     function modificar($id)
     {
+        $p = Prestamo::find($id);
+        $libros = Libro::all();
+        return view('modificarP', compact('p', 'libros'));
     }
 
     function insertar(Request $r)
@@ -34,5 +39,47 @@ class PrestamoC extends Controller
             'libro' => 'required',
             'cliente' => 'required',
         ]);
+        //Chequear ejemplares
+        $l = Libro::find($r->libro);
+        if ($l != null and $l->numEjemplares > 0) {
+            //Chequear que el cliente no tiene préstamos pendientes
+            $pendientes = Prestamo::where('nombreCliente', $r->cliente)
+                ->where('fechaDevolucion', null)->get();
+            if (sizeof($pendientes) > 0) {
+                return back()->with('mensaje', 'Error, el cliente tiene libros sin devolver');
+            } else {
+                //Registrar préstamos y actualizar nº de ejemplares
+                $error = false;
+                $mensaje = "";
+                try {
+                    DB::transaction(function () use ($r, $error) {
+                        //Insert
+                        $p = new Prestamo();
+                        $p->fecha = $r->fecha;
+                        $p->libro_id = $r->libro;
+                        $p->nombreCliente = $r->cliente;
+                        if ($p->save()) {
+                            //Modificar el nº de ejemplares
+                            $p->libro->numEjemplares = $p->libro->numEjemplares - 1;
+                            if (!$p->libro->save()) {
+                                $error = true;
+                            }
+                        } else {
+                            $error = true;
+                        }
+                    });
+                } catch (Exception $e) {
+                    $mensaje = $e->getMessage();
+                } finally {
+                    if ($error) {
+                        return back()->with('mensaje', $mensaje);
+                    } else {
+                        return redirect()->route('rutaVer');
+                    }
+                }
+            }
+        } else {
+            return back()->with('mensaje', 'Error, libro no existe o no hay ejemplares');
+        }
     }
 }
--- u4_ExBiblio/resources/views/modificarP.blade.php
+++ u4_ExBiblio/resources/views/modificarP.blade.php
@@ -7,15 +7,13 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>PRÉSTAMOS</h1>
-    <form action="{{route('rutaInsertar')}}" method="POST">
+    <h1>MODIFICAR PRÉSTAMO</h1>
+    <form action="{{route('rutaActualizar', $p->id)}}" method="POST">
     @csrf
+    <label for="Id">Id</label>
+    <input type="number" name="id" disabled="disabled" value="{{$p->id}}">
     <label for="fecha">Fecha</label>
-@if (old('fecha')!=null)
-    <input type="date" name="fecha" value="{{old('fecha')}}">
-    @else
-<input type="date" name="fecha" value="{{date('Y-m-d')}}">
-    @endif
+    <input type="date" name="fecha" value="{{$p->fecha}}">
     @error('fecha')
         <div>
             Rellena Fecha
@@ -25,7 +23,7 @@
     <label for="libro">Libro</label>
     <select name="libro" id="libro">
         @foreach ($libros as $l)
-        @if(old('libro')!=null and old('libro')==$l->id)
+        @if($p->id==$l->id)
         <option value="{{$l->id}}" selected="selected">{{$l->titulo}}</option>
         @else
         <option value="{{$l->id}}">{{$l->titulo}}</option>
@@ -39,15 +37,18 @@
     @enderror
 
     <label for="cliente">Cliente</label>
-    <input type="text" name="cliente" value="{{old('cliente')}}">
+    <input type="text" name="cliente" value="{{$p->nombreCliente}}">
     @error('cliente')
         <div>
             Rellena Cliente
         </div>
     @enderror
 
-    <button type="submit">Crear</button>
-    <button type="reset">Limpiar</button>
+        <label for="fechaD">Fecha Devolución</label>
+    <input type="date" name="fechaD" value="{{$p->fechaDevolucion}}">
+
+    <button type="submit">Modificar</button>
+    <a href="{{route('rutaVer')}}">Cancelar</a>
 </form>
 <div style="color:red">
     @if(session('mensaje')!=null)
